0dee90d90a5e8bdf0b2d67d2908a19eaaee19a38
RobertHan96 <yshan4329@gmail.com>
2019-10-07 09:12:19 +0900

    2nd commit : 강좌 내용 모두 구현 완료 (CURD)

--- students/urls.py
+++ students/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
-urlpatterns = (
-    path('admin/', admin.site.urls),
-    path('students/', include('students.urls')),
-)
+app_name = 'students'
+urlpatterns = [
+    path('reg/', views.regStudent, name='reg'),
+    path('regCon/', views.regConStudent, name='regCon'),
+    path('all/', views.reaStudentAll, name='stuAll'),
+    path('<str:name>det/', views.detStudent, name='stuDet'),
+    path('<str:name>mod/', views.reaStudentOne, name='stuMod'),
+    path('modCon/', views.modConStudent, name='modCon'),
+    path('<str:name>del/', views.delStudent, name='stuDel'),
+
+]
